41bb31c8a5699dd2b43d63c94cb741cbb81b5373
Frank151001 <124334022+FranciscoIbarraG@users.noreply.github.com>
2023-03-22 14:04:25 -0700

    Just checking

--- test3.py
+++ test3.py
@@ -6,3 +6,12 @@ for color in colors:
     color_lower = color.lower()
     if color_lower not in results:
         results.append(color_lower)
+print(results)
+
+
+color="red"
+count=0
+for c in colors:
+    if color==c.lower():
+        count= count+1
+print(count)
